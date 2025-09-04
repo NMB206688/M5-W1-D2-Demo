1d04b6950f192d3e9365e1d5a0f4f4b5d51d4224
Naveen Mamidi <naveenmamidi6798@gmail.com>
2025-09-03 21:00:14 -0700

    M5-W1-D2-demo

--- demo5.js
+++ demo5.js
@@ -1,15 +1,19 @@
 class Football extends React.Component {
-    shoot = () => {
-    alert(this);
-    /*
-    The 'this' keyword refers to the component object
-    */
-    }
-    render() {
-    return (
-    <button onClick={this.shoot}>Take the shot!</button>
-    );
-    }
-    }
 
+  shoot = () => {
+    alert("Goal! This refers to the component: " + this.constructor.name);
+    /*
+      - 'this' is bound to the Football component instance
+      - Using this.constructor.name gives the class name
+    */
+  }
+
+  render() {
+    return (
+      <button onClick={this.shoot}>Take the shot!</button>
+    );
+  }
+}
+
+// ✅ Render AFTER the class is defined
 ReactDOM.render(<Football />, document.getElementById('root'));
